7e8b1c4366f9dfe14ce2ced5819a1d89c78d8393
CHAE <chaso1495@naver.com>
2023-03-06 21:46:20 +0900

    [feat] Study Week 1

--- Graph/7576.cpp
+++ Graph/7576.cpp
@@ -2,26 +2,25 @@
 #include <queue>
 
 using namespace std;
-
-struct Dot {
-    int x;
-    int y;
-    int day;
-
-    Dot(int inputx, int inputy, int d){
-        x = inputx;
-        y = inputy;
-        day = d;
-    }
-};
-
-int m, n;
 int map[1001][1001];
+int m, n;
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
-queue<Dot> q;
+queue<pair<pair<int, int>, int> > q;
 
-bool validcheck() {
+void input(){
+    cin >> m >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> map[i][j];
+            if(map[i][j] == 1){
+                q.push(make_pair(make_pair(i, j), 0));
+            }
+        }
+    }
+}
+
+bool validcheck(){
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             if(map[i][j] == 0) return false;
@@ -30,44 +29,37 @@ bool validcheck() {
     return true;
 }
 
-void BFS() {
-    int totalday = 0;
+void BFS(){
+    int total = 0;
 
     while(!q.empty()){
-        Dot dot = q.front();
+        int x = q.front().first.first;
+        int y = q.front().first.second;
+        int cnt = q.front().second;
+
+        total = cnt;
         q.pop();
-        totalday = dot.day;
 
         for(int i=0; i<4; i++){
-            int nx = dot.x + dx[i];
-            int ny = dot.y + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
             if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-
             if(map[nx][ny] == 0){
                 map[nx][ny] = 1;
-                q.push(Dot(nx, ny, dot.day + 1));
+                q.push(make_pair(make_pair(nx, ny), total + 1));
             }
         }
     }
-
     if(validcheck()){
-        cout << totalday << '\n';
+        cout << total;
     } else {
-        cout << -1 << '\n';
+        cout << -1;
     }
 }
 
-int main() {
-    cin >> m >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin >> map[i][j];
-            if(map[i][j] == 1){
-                q.push(Dot(i, j, 0));
-            }
-        }
-    }
+int main(){
+    input();
     BFS();
     return 0;
 }
