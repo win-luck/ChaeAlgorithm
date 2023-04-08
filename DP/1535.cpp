0e884290cafb6cc5a8c7540ddb8ffae60ecf0081
CHAE <chaso1495@naver.com>
2023-04-08 09:32:33 +0900

    [feat] 1535 DP

--- DP/1535.cpp
+++ DP/1535.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 int n;
-int dp[21][101]; // N명의 사람까지 만날 수 있을 때 100의 체력으로 얻은 최대 기쁨
+int dp[21][101]; // N명의 사람까지 만날 수 있을 때 100의 체력으로 얻은 최대 점수
 int power[21];
 int please[21];
 int m = 0;
@@ -23,13 +23,13 @@ int main(){
 
     for(int i=1; i<=n; i++){
         for(int j=100; j>=0; j--){
-            if(j-power[i]>=1){
+            if(j-power[i]>=1){ // 경계값이 0이면 게임오버
                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-power[i]] + please[i]);
             } else {
                 dp[i][j] = dp[i-1][j];
             }
 
-            m = max(m, dp[i][j]);
+            m = max(m, dp[i][j]); // 최대 점수 갱신
         }
     }
     cout << m << '\n';
