bf2aa05dc5251664041faa9e89f0eb25a14ed75c
CHAE <chaso1495@naver.com>
2023-04-08 09:31:14 +0900

    [feat] 1535 DP

---
+++ DP/1535.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int n;
+int dp[21][101]; // N명의 사람까지 만날 수 있을 때 100의 체력으로 얻은 최대 기쁨
+int power[21];
+int please[21];
+int m = 0;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for(int i=1; i<=n; i++){ // 잃을 체력
+        cin >> power[i];
+    }
+    for(int i=1; i<=n; i++){ // 얻을 점수
+        cin >> please[i];
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=100; j>=0; j--){
+            if(j-power[i]>=1){
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-power[i]] + please[i]);
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+
+            m = max(m, dp[i][j]);
+        }
+    }
+    cout << m << '\n';
+    return 0;
+}
