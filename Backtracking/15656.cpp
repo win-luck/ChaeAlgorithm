d1794e1e070f6aa09d41589962b51d602d00acba
CHAE <chaso1495@naver.com>
2023-04-13 11:19:55 +0900

    [feat] 15656 Backtracking

---
+++ Backtracking/15656.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int n, m;
+int arr[9];
+bool visited[9];
+vector<int> v;
+
+void backtracking(int cnt){
+    if(cnt == m){
+        for(int i=0; i<v.size(); i++){
+            cout << v[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=0; i<n; i++){
+        if(visited[i]) continue;
+        if(v.empty() || v[v.size()-1] < arr[i]){
+            visited[i] = true;
+            v.push_back(arr[i]);
+            backtracking(cnt + 1);
+            v.pop_back();
+            visited[i] = false;
+        }
+    }
+    
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+    backtracking(0);
+}
