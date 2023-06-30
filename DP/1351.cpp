f22fbde062e82f04c879aef10aebba195c2e4ff6
CHAE <chaso1495@naver.com>
2023-06-30 09:40:18 +0900

    [feat] 1351 DP

---
+++ DP/1351.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+long long n, p, q;
+map<long long, long long> m; // 탐색범위가 지나치게 크므로 map으로 처리
+
+long long recur(long long n){ // 메모이제이션 활용
+    if(n == 0) return 1;
+    if(m.count(n) > 0){ // 이미 존재하면 반환
+        return m[n];
+    }
+    if(n/p == n/q){ // 둘이 동일하면 계산 두 번 할 필요 없게
+        return m[n] = 2 * recur(n/p);
+    }
+    return m[n] = recur(n/p) + recur(n/q);
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> n >> p >> q;
+    cout << recur(n) << '\n';
+    return 0;
+}
