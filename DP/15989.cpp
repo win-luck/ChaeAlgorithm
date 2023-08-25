0f2352babc8d04b009dc44a4e43f094200afa864
CHAE <chaso1495@naver.com>
2023-08-25 10:05:11 +0900

    [feat] 15989 DP

--- DP/15989.cpp
+++ DP/15989.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 
 using namespace std;
-int n;
-long long dp[36];
+int n, t;
+int dp[10001][4] = {0,}; // 정수 i를 만들 때 마지막으로 더한 수가 1~3인 경우의 수
 
 int main(){
-    cin >> n;
-    dp[0] = 1;
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 5;
-    for(int i=4; i<=n; i++){
-        for(int j=0; j<i; j++){
-            dp[i] += (dp[j] * dp[i-1-j]);
-        }
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> t;
+    dp[1][1] = 1;
+    dp[2][1] = 1;
+    dp[3][1] = 1;
+    dp[2][2] = 1;
+    dp[3][2] = 1;
+    dp[3][3] = 1;
+    for(int i=4; i<=10000; i++){ // 중복 회피를 위해 오름차순으로만 더할 수 있다 가정하자
+        dp[i][1] = dp[i-1][1];
+        dp[i][2] = dp[i-2][1] + dp[i-2][2];
+        dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
     }
-    cout << dp[n] << '\n';
+    while(t--){
+        cin >> n;
+        cout << dp[n][1] + dp[n][2] + dp[n][3] << '\n';
+    }
     return 0;
 }
