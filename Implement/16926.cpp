178457b2db2dad099310bdd4912ddb35f0f7e99b
CHAE <chaso1495@naver.com>
2024-01-27 14:28:37 +0900

    [PS] 16926 Imple

---
+++ Implement/16926.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+int n, m, r;
+int arr[301][301];
+bool visited[301][301];
+
+void print(){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << '\n';
+    }
+}
+
+void rotate(int sx, int sy, int lenx, int leny){
+    if(visited[sx][sy]) return;
+
+    // 위쪽 (<-)
+    int tmp = arr[sx][sy];
+    visited[sx][sy] = true;
+    for(int i=sy; i<sy+leny-1; i++){
+        arr[sx][i] = arr[sx][i+1];
+        visited[sx][i] = true;
+    }
+
+    // 오른쪽 (업)
+    int ypos = sy+leny-1;
+    for(int i=sx; i<sx+lenx-1; i++){
+        arr[i][ypos] = arr[i+1][ypos];
+        visited[i][ypos] = true;
+    }
+
+    // 아래쪽 (->)
+    int xpos = sx+lenx-1;
+    for(int i=ypos; i>sy; i--){
+        arr[xpos][i] = arr[xpos][i-1];
+        visited[xpos][i] = true;
+    }
+
+    // 왼쪽 (다운)
+    for(int i=xpos; i>sx+1; i--){
+        arr[i][sy] = arr[i-1][sy];
+        visited[i][sy] = true;
+    }
+
+    arr[sx+1][sy] = tmp;
+    visited[sx+1][sy] = true;
+    rotate(sx+1, sy+1, lenx-2, leny-2); // 다음 출발점
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m >> r;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    while(r--){
+        rotate(0, 0, n, m);
+        memset(visited, false, sizeof(visited));
+    }
+    print();
+    return 0;
+}
