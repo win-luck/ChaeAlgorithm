8dcca7cd84169259c9f7e7b99e33090cde8d0dcd
CHAE <chaso1495@naver.com>
2024-05-26 21:37:17 +0900

    [PS] 체육복

--- programmers/체육복.cpp
+++ programmers/체육복.cpp
@@ -15,19 +15,19 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
         arr[lost[i]]--;
     }
     for(int i=1; i<=n; i++){
-        if(arr[i] == 1){
-            if(arr[i-1] == -1){
+        if(arr[i] == 1){ // 현재 여벌
+            if(arr[i-1] == -1){ // 왼쪽이 여벌 필요
                 arr[i-1]++;
                 arr[i]--;
-                answer += 2;
-            } else if(arr[i+1] == -1){
+                answer += 2; // 다시 왼쪽 친구 순회할 일 없기에 +2
+            } else if(arr[i+1] == -1){ // 오른쪽이 여벌 필요
                 arr[i+1]++;
                 arr[i]--;
-                answer++;
+                answer++; // 오른쪽은 다음 인덱스에서 반영
             } else {
                 answer++;
             }
-        } else if(arr[i] == 0){
+        } else if(arr[i] == 0){ // 자기만 입음
             answer++;   
         }
     }
